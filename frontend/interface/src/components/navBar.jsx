3e34fe3e8c7b9b4ecdd46c340398cc95515451a5
andrewhxd <andrewhxue@gmail.com>
2025-11-12 18:16:13 -0600

    added shadcn

--- frontend/interface/src/components/navBar.jsx
+++ frontend/interface/src/components/navBar.jsx
@@ -1,12 +1,13 @@
 import './navBar.css';
 import logo from '../assets/nfr_logo.png';
+import ShinyText from './ui/ShinyText';
 
 export default function Navbar() {
     return (
         <nav className="navbar">
             <div class="navbar-left">
                 <img src={logo} alt="Logo" className="navbar-logo" />
-                <h1 class="navbar-title">NFR Interface</h1>
+                <ShinyText text="NFR Interface" className="navbar-title" />
             </div>
             <ul className="navbar-links">
                 <li><a href="/">Home</a></li>
