84603ffe50b580e6c14cabc4286d5a3e5d150f53
andrewhxd <andrewhxue@gmail.com>
2026-05-13 22:10:16 -0500

    delete the old Dash/Graphs/Replay pages, redirect them to /app, also drop a bunch of unused deps

--- frontend/interface/src/App.jsx
+++ frontend/interface/src/App.jsx
@@ -2,9 +2,6 @@ import './App.css'
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import Home from './pages/Home';
-import Dash from './pages/Dash';
-import Replay from './pages/Replay';
-import Graphs from './pages/Graphs';
 import AppDownload from './pages/AppDownload';
 import TopBar from './components/TopBar';
 import AppDevRoute from './routes/AppDevRoute';
@@ -34,9 +31,6 @@ function App() {
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-          <Route path="/dashboard" element={<PageWrapper><Dash /></PageWrapper>} />
-          <Route path="/replay" element={<PageWrapper><Replay /></PageWrapper>} />
-          <Route path="/graphs" element={<PageWrapper><Graphs /></PageWrapper>} />
           <Route path="/app-download" element={<PageWrapper><AppDownload /></PageWrapper>} />
           <Route path="/app" element={<AppRoute />} />
           <Route path="/app-dev" element={<AppDevRoute />} />
